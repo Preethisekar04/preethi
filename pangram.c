4665e69fec4f8784fa8240e25f5bfcaeb60b2017
Preethisekar04 <preethisekar04@gmail.com>
2016-09-02 12:14:50 +0530

    Create pangram.c

---
+++ pangram.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int index,i,pangram;
+char c,x[26]={0};
+while((ch=getchar())!='\n')
+{
+if('A'<=ch&&ch<='z')
+index=ch-'a';
+else
+continue
+x[index]=1;
+}
+pangram=1;
+for(i=0;i<26;++i)
+{
+if(x[i]==0)
+pangram=0;
+}
+if(pangram==0)
+printf("pangram");
+else
+if("not");
+getch(0;
+}
